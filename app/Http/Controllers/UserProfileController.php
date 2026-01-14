47e88e8d58b0f6eccff5ad9789f3dd27148e0462
გიო <giodevadze01@gmail.com>
2026-01-14 17:57:17 +0400

    updates

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
 class UserProfileController extends Controller
